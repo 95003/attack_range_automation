da557fc82b4e3f72c4dffc60890d228367ff4867
95003 <dhaarun25052004@gmail.com>
2025-09-11 18:12:42 +0530

    Created .tfvars.example file

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-*"]
   }
 
   filter {
