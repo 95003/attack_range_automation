fd936d12aba7834d34fa1bd79f207a510dabedb7
95003 <dhaarun25052004@gmail.com>
2025-09-13 14:15:48 +0530

    Modified my main.tf file ubuntu version from 22 to 24

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
   }
 
   filter {
