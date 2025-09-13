49d03ed8e2345c8747eb871178fd70f3bac24cde
95003 <dhaarun25052004@gmail.com>
2025-09-13 14:13:44 +0530

    Modified my main.tf file ubuntu version from 22 to 24

--- main.tf
+++ main.tf
@@ -19,12 +19,11 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-# --- Random ID to avoid SG name conflicts ---
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# --- Get Latest Ubuntu 22.04 AMI ---
+# --- Get Latest Ubuntu 24.04 AMI ---
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical official
@@ -40,7 +39,6 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# --- EC2 Instance ---
 resource "aws_instance" "attack_range" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.medium"
@@ -56,20 +54,17 @@ resource "aws_instance" "attack_range" {
   }
 }
 
-# --- Security Group ---
 resource "aws_security_group" "attack_range_sg" {
   name        = "attack-range-sg-${random_id.suffix.hex}"
   description = "Security group for attack-range-server"
 
-  # SSH
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # replace with your IP for safety
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP
   ingress {
     from_port   = 80
     to_port     = 80
@@ -77,7 +72,6 @@ resource "aws_security_group" "attack_range_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTPS
   ingress {
     from_port   = 443
     to_port     = 443
@@ -85,7 +79,6 @@ resource "aws_security_group" "attack_range_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Custom TCP 8000–9999
   ingress {
     from_port   = 8000
     to_port     = 9999
@@ -93,7 +86,6 @@ resource "aws_security_group" "attack_range_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound
   egress {
     from_port   = 0
     to_port     = 0
@@ -102,18 +94,15 @@ resource "aws_security_group" "attack_range_sg" {
   }
 }
 
-# Attach SG to EC2
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
   security_group_id    = aws_security_group.attack_range_sg.id
   network_interface_id = aws_instance.attack_range.primary_network_interface_id
 }
 
-# --- Output Public IP ---
 output "attack_range_public_ip" {
   value = aws_instance.attack_range.public_ip
 }
 
-# --- Create Ansible Inventory File ---
 resource "local_file" "ansible_inventory" {
   filename = "${path.module}/inventory.ini"
 
